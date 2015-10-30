5b1bf0326bfbbbb665f496042d40bc4d4ba0b66d
Sarwo Dwi Waskito <sarwo@rotterdam-cs.com>
2015-10-30 15:42:54 +0700

    IND role constants

--- src/main/java/com/rcs/ind/common/IndConstants.java
+++ src/main/java/com/rcs/ind/common/IndConstants.java
@@ -9,4 +9,10 @@ public class IndConstants {
 	public static final int UNDEFINED = -1;
 	public static final String DATE_FORMAT = "dd-MM-yyyy";
 	public static final String UNKNOWN_ERROR_KEY = "com.rcs.common.error.unknown";
+	
+	// IND role constants
+	public static final String EMPLOYEE_ROLE_NAME = "Employee";
+	public static final String INTERVIEWER_ROLE_NAME = "Interviewer";
+	public static final String SUPERVISOR_ROLE_NAME = "Supervisor";
+	public static final String TRANSLATOR_ROLE_NAME = "Translator";
 }
