ad584c059c6e32f3309dd633db0d1c5922645cbc
M Fauzan Abdi <fauzan.abdi@rotterdam-cs.com>
2015-11-04 13:10:37 +0700

    #21548: add more elements into applicationGridDTO

--- src/main/java/com/rcs/ind/common/dtos/ApplicationGridDTO.java
+++ src/main/java/com/rcs/ind/common/dtos/ApplicationGridDTO.java
@@ -1,10 +1,16 @@
 package com.rcs.ind.common.dtos;
 
+import java.util.Date;
+import java.util.Locale;
+
 
 public class ApplicationGridDTO extends GenericEntityDTO {
 	
 	private Long applicationId;
 	private String smartflowNumber;
+	private Date startDate;
+	private Date endDate;
+	private Locale language;
 	
 	public Long getApplicationId() {
 		return applicationId;
@@ -22,5 +28,32 @@ public class ApplicationGridDTO extends GenericEntityDTO {
 	
 	public void setSmartflowNumber(String smartflowNumber) {
 		this.smartflowNumber = smartflowNumber;
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	public Locale getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(Locale language) {
+		this.language = language;
 	}
 }
