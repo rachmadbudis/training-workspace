c9402dcba0914fe7d65c09df8a0943bea5505e31
ryusuf <ridwan.yusuf@rotterdam-cs.com>
2015-11-05 13:41:05 +0700

    checkpoint interview my page + code review

--- src/main/java/com/rcs/ind/common/filters/dtos/ApplicationHistoryFiltersDTO.java
+++ src/main/java/com/rcs/ind/common/filters/dtos/ApplicationHistoryFiltersDTO.java
@@ -1,67 +1,67 @@
 package com.rcs.ind.common.filters.dtos;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import com.rcs.ind.common.dtos.PaginationFiltersDTO;
 import com.rcs.ind.common.enums.ApplicationStatusEnum;
 
-
 public class ApplicationHistoryFiltersDTO extends PaginationFiltersDTO {
-	
+
 	private Long applicationHistoryId;
-	private ApplicationStatusEnum status;
+	private List<ApplicationStatusEnum> statuses;
 	private Date startDate;
 	private Date endDate;
 	private Long applicationId;
 	private String smartflowNumber;
 	private Locale language;
 	private Long assignedUserId;
-	
+
 	public Long getApplicationHistoryId() {
 		return applicationHistoryId;
 	}
-	
+
 	public void setApplicationHistoryId(Long applicationHistoryId) {
 		this.applicationHistoryId = applicationHistoryId;
 	}
-	
-	public ApplicationStatusEnum getStatus() {
-		return status;
+
+	public List<ApplicationStatusEnum> getStatuses() {
+		return statuses;
 	}
-	
-	public void setStatus(ApplicationStatusEnum status) {
-		this.status = status;
+
+	public void setStatuses(List<ApplicationStatusEnum> statuses) {
+		this.statuses = statuses;
 	}
-	
+
 	public Date getStartDate() {
 		return startDate;
 	}
-	
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
-	
+
 	public Date getEndDate() {
 		return endDate;
 	}
-	
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
-	
+
 	public Long getApplicationId() {
 		return applicationId;
 	}
-	
+
 	public void setApplicationId(Long applicationId) {
 		this.applicationId = applicationId;
 	}
-	
+
 	public Long getAssignedUserId() {
 		return assignedUserId;
 	}
-	
+
 	public void setAssignedUserId(Long assignedUserId) {
 		this.assignedUserId = assignedUserId;
 	}
@@ -81,5 +81,4 @@ public class ApplicationHistoryFiltersDTO extends PaginationFiltersDTO {
 	public void setLanguage(Locale language) {
 		this.language = language;
 	}
-	
 }
