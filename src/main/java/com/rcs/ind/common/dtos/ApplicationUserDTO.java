dbf0ed013e8ab0867b079d5baa32f7136151d5fd
Sarwo Dwi Waskito <sarwo@rotterdam-cs.com>
2015-11-19 14:20:39 +0700

    #21707: Add a DTO for assign application

---
+++ src/main/java/com/rcs/ind/common/dtos/ApplicationUserDTO.java
@@ -0,0 +1,33 @@
+package com.rcs.ind.common.dtos;
+
+public class ApplicationUserDTO {
+
+	private Long applicationId;
+	private Long userId;
+	private String userName;
+
+	public Long getApplicationId() {
+		return applicationId;
+	}
+
+	public void setApplicationId(Long applicationId) {
+		this.applicationId = applicationId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
