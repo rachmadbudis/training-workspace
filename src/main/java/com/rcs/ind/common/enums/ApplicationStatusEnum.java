d528290948c234f884248170de43b6b72b56d72c
Iman L Hakim <iman.hakim@rotterdam-cs.com>
2015-11-24 16:18:25 +0700

    add empty status

--- src/main/java/com/rcs/ind/common/enums/ApplicationStatusEnum.java
+++ src/main/java/com/rcs/ind/common/enums/ApplicationStatusEnum.java
@@ -1,6 +1,7 @@
 package com.rcs.ind.common.enums;
 
 public enum ApplicationStatusEnum {
+	Empty("com.rcs.ind.application.status.empty"),
 	CreateInProgress("com.rcs.ind.application.status.create.in.progress"),
 	New("com.rcs.ind.application.status.new"),
 	TranslateInProgress("com.rcs.ind.application.status.translate.in.progress"),
