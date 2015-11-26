7a5a3e53e4823ab1091f969e02c02a272bc414d0
M Fauzan Abdi <fauzan.abdi@rotterdam-cs.com>
2015-11-26 11:12:52 +0700

    add application Release Reason Enum

---
+++ src/main/java/com/rcs/ind/common/enums/ApplicationReleaseReasonEnum.java
@@ -0,0 +1,18 @@
+package com.rcs.ind.common.enums;
+
+public enum ApplicationReleaseReasonEnum {
+	Iliterate("com.rcs.ind.application.release.reason.iliterate"),
+	ComputerIliterate("com.rcs.ind.application.release.reason.computer.iliterate"),
+	Leave("com.rcs.ind.application.release.reason.leave");
+
+	private final String key;
+
+	private ApplicationReleaseReasonEnum(String key) {
+		this.key = key;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+}
