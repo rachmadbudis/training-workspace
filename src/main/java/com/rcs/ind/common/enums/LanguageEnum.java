bdbe5a97ba6cea0b66b0578901a5f8933c12004a
M Fauzan Abdi <fauzan.abdi@rotterdam-cs.com>
2015-11-27 14:55:09 +0700

    #21779: update language & location Enum

--- src/main/java/com/rcs/ind/common/enums/LanguageEnum.java
+++ src/main/java/com/rcs/ind/common/enums/LanguageEnum.java
@@ -1,12 +1,17 @@
 package com.rcs.ind.common.enums;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public enum LanguageEnum {
 	ARABIC(new Locale("ar", "SY")),
 	DUTCH(new Locale("nl", "NL"));
 
 	private final Locale language;
+	private static LanguageEnum[] vals = values();
 
 	private LanguageEnum(Locale language) {
 		this.language = language;
@@ -23,4 +28,16 @@ public enum LanguageEnum {
 	public static Locale getDefaultLocale() {
 		return LanguageEnum.DUTCH.getLanguage();
 	}
+
+	public static List<Map<String, String>> getAutoCompleteLanguages() {
+		List<Map<String, String>> list = new ArrayList<>();
+		for (LanguageEnum languageEnum : vals) {
+			Map<String, String> tModel = new HashMap<>();
+			tModel.put("value", languageEnum.getLanguage().toString());
+			tModel.put("label", languageEnum.getDisplayLanguage());
+			list.add(tModel);
+		}
+		return list;
+	}
+
 }
--- src/main/java/com/rcs/ind/common/enums/LocationEnum.java
+++ src/main/java/com/rcs/ind/common/enums/LocationEnum.java
@@ -1,11 +1,17 @@
 package com.rcs.ind.common.enums;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public enum LocationEnum {
 	IND_LOKET_ROTTERDAM("IND Loket Rotterdam"),
 	RIJSWIJK("Rijswijk"),
 	TER_APEL("Ter Apel");
 
 	private final String location;
+	private static LocationEnum[] vals = values();
 
 	private LocationEnum(String location) {
 		this.location = location;
@@ -18,4 +24,16 @@ public enum LocationEnum {
 	public LocationEnum getDefaultLocation() {
 		return IND_LOKET_ROTTERDAM;
 	}
+
+	public static List<Map<String, String>> getAutoCompleteLocations() {
+		List<Map<String, String>> list = new ArrayList<>();
+		for (LocationEnum locationEnum : vals) {
+			Map<String, String> tModel = new HashMap<>();
+			tModel.put("value", locationEnum.name());
+			tModel.put("label", locationEnum.getLocation());
+			list.add(tModel);
+		}
+		return list;
+	}
+
 }
