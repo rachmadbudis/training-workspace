741ea255161850c90a94dca05bb4004d982590ad
Iman L Hakim <iman.hakim@rotterdam-cs.com>
2015-12-22 11:21:52 +0700

    add nationality enum

---
+++ src/main/java/com/rcs/ind/common/enums/NationalityEnum.java
@@ -0,0 +1,13 @@
+package com.rcs.ind.common.enums;
+
+
+public enum NationalityEnum {
+	Syrian("Syrian"),
+	Dutch("Dutch");
+
+	private final String key;
+
+	private NationalityEnum(String key) {
+		this.key = key;
+	}	
+}
