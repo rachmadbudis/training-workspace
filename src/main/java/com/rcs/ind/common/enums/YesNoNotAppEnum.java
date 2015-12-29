089d3ece82338e3f41039753e2afd3773942d0b7
M Fauzan Abdi <fauzan.abdi@rotterdam-cs.com>
2015-12-29 14:28:36 +0700

    #22006: upadate key

--- src/main/java/com/rcs/ind/common/enums/YesNoNotAppEnum.java
+++ src/main/java/com/rcs/ind/common/enums/YesNoNotAppEnum.java
@@ -1,13 +1,17 @@
 package com.rcs.ind.common.enums;
 
 public enum YesNoNotAppEnum {
-	Yes("Yes"),
-	No("No"),
-	NotApplicable("NotApplicable");
+	Yes("com.rcs.ind.common.yes"),
+	No("com.rcs.ind.common.no"),
+	NotApplicable("com.rcs.ind.preparation.dublin.dactyl.not.applicable");
 
 	private final String key;
 
 	private YesNoNotAppEnum(String key) {
 		this.key = key;
+	}
+	
+	public String getKey() {
+		return key;
 	}
 }
