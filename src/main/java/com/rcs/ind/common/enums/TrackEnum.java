76515b075ada167d01a1c754e14083dfa4f55039
ryusuf <ridwan.yusuf@rotterdam-cs.com>
2016-01-05 16:38:12 +0700

    add track #22046

--- src/main/java/com/rcs/ind/common/enums/TrackEnum.java
+++ src/main/java/com/rcs/ind/common/enums/TrackEnum.java
@@ -1,9 +1,14 @@
 package com.rcs.ind.common.enums;
 
 public enum TrackEnum {
-	Dublin("Dublin"),
-	Standard("Standard");
-
+	Dublin("com.rcs.ind.track.dublin"),
+	ProtectedCountry("com.rcs.ind.track.protected.country"),
+	SafeCountry("com.rcs.ind.track.safe.country"),
+	NationalitySyErValid("com.rcs.ind.track.nationality.sy.er.valid"),
+	NationalityElse("com.rcs.ind.track.nationality.else"),
+	NationalitySyErInvalid("com.rcs.ind.track.nationality.sy.er.invalid"),
+	;
+	
 	private final String key;
 
 	private TrackEnum(String key) {
