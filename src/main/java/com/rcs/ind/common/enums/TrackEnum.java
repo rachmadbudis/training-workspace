d6d7581115c34e6a06dc84737c89d174c3b83f01
Sarwo Dwi Waskito <sarwo@rotterdam-cs.com>
2016-02-05 15:03:29 +0700

    #22272: remove valide word on Track 3 and 5

--- src/main/java/com/rcs/ind/common/enums/TrackEnum.java
+++ src/main/java/com/rcs/ind/common/enums/TrackEnum.java
@@ -4,9 +4,9 @@ public enum TrackEnum {
 	Dublin("com.rcs.ind.track.dublin", "1. Dublin"),
 	ProtectedCountry("com.rcs.ind.track.protected.country", "2. Beschermd door EU land"),
 	SafeCountry("com.rcs.ind.track.safe.country", "2. Veilig land"),
-	NationalitySyErValid("com.rcs.ind.track.nationality.sy.er.valid", "3. Nationaliteit (Syrische of Eritreese) (Valide id-documenten aangeleverd)"),
+	NationalitySyErValid("com.rcs.ind.track.nationality.sy.er.valid", "3. Nationaliteit (Syrische of Eritreese) (Id-documenten aangeleverd)"),
 	NationalityElse("com.rcs.ind.track.nationality.else", "4. Overige Nationaliteiten"),
-	NationalitySyErInvalid("com.rcs.ind.track.nationality.sy.er.invalid", "5. Nationaliteit (Syrische of Eritreese) (Geen valide id-documenten aangeleverd)"),
+	NationalitySyErInvalid("com.rcs.ind.track.nationality.sy.er.invalid", "5. Nationaliteit (Syrische of Eritreese) (Geen id-documenten aangeleverd)"),
 	;
 	
 	private final String key;
