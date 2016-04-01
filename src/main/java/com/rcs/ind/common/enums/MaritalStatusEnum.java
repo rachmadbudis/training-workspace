71d92bb30dee074e700a08a60b66c8fb21dd3028
Sarwo Dwi Waskito <sarwo@rotterdam-cs.com>
2016-04-01 13:39:52 +0700

    #22683: add new option for marital status

--- src/main/java/com/rcs/ind/common/enums/MaritalStatusEnum.java
+++ src/main/java/com/rcs/ind/common/enums/MaritalStatusEnum.java
@@ -4,7 +4,8 @@ public enum MaritalStatusEnum {
 	Single("com.rcs.ind.marital.single"),
 	Married("com.rcs.ind.marital.merried"),
 	Divorced("com.rcs.ind.marital.divorced"),
-	Widowed("com.rcs.ind.marital.widowed");
+	Widowed("com.rcs.ind.marital.widowed"),
+	LivingTogether("com.rcs.ind.marital.living.together");
 
 	private final String key;
 
