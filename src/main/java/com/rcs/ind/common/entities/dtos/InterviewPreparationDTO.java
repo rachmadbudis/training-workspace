b36aea515ecadb361fa6fcb08ac846070a6eeea5
Sarwo Dwi Waskito <sarwo@rotterdam-cs.com>
2016-04-04 14:48:05 +0700

    #22682 and #22686: add new location added attribute for interview preparation and remove 'beedigd vertaler' from registered interpreter enum

--- src/main/java/com/rcs/ind/common/entities/dtos/InterviewPreparationDTO.java
+++ src/main/java/com/rcs/ind/common/entities/dtos/InterviewPreparationDTO.java
@@ -53,6 +53,7 @@ public class InterviewPreparationDTO {
 	private List<PreparationDocumentDTO> documents;
 	private Map<String, String> data;
 	private String interviewDocument;
+	private String locationAdded;
 
 	public InterviewPreparationDTO() {
 	}
@@ -451,6 +452,14 @@ public class InterviewPreparationDTO {
 
 	public void setInterviewDocument(String interviewDocument) {
 		this.interviewDocument = interviewDocument;
+	}
+
+	public String getLocationAdded() {
+		return locationAdded;
+	}
+
+	public void setLocationAdded(String locationAdded) {
+		this.locationAdded = locationAdded;
 	}
 
 }
