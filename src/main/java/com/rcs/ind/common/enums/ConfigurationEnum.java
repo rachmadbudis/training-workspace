afa16b3f27af745d438f1d9d4912c6fb53873f70
M Fauzan Abdi <fauzan.abdi@rotterdam-cs.com>
2016-04-15 10:51:02 +0700

    add ddl to configurationenum

--- src/main/java/com/rcs/ind/common/enums/ConfigurationEnum.java
+++ src/main/java/com/rcs/ind/common/enums/ConfigurationEnum.java
@@ -2,7 +2,11 @@ package com.rcs.ind.common.enums;
 
 public enum ConfigurationEnum {
 	EmailSender(1, "emailSender", "changethis@ind.com"),
-	EmailReceiver(2, "emailReceiver", "changethis@ind.com");
+	EmailReceiver(2, "emailReceiver", "changethis@ind.com"),
+	NationalitiesDDL(3, "nationalitiesDDL", "0"),
+	LanguageDDL(4, "languageDDL", "0"),
+	ClaimlandDDL(5, "claimlandDDL", "0"),
+	LocationDDL(6, "locationDDL", "0");
 
 	private final int id;
 	private final String key;
