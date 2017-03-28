c871d018f33b5f32e2fa253ec509368262b2603d
daniel <daniel@rotterdam-cs.com>
2017-03-28 16:24:57 +0200

    issue 3.0.9.110

--- src/main/java/com/rcs/ind/common/enums/TrackEnum.java
+++ src/main/java/com/rcs/ind/common/enums/TrackEnum.java
@@ -1,24 +1,30 @@
 package com.rcs.ind.common.enums;
 
 public enum TrackEnum {
-	Dublin("com.rcs.ind.track.dublin"),
-	ProtectedCountry("com.rcs.ind.track.protected.country"),
-	SafeCountry("com.rcs.ind.track.safe.country"),
-	NationalitySyErValid("com.rcs.ind.track.nationality.sy.er.valid"),
-	NationalityElse("com.rcs.ind.track.nationality.else"),
-	NationalitySyErInvalid("com.rcs.ind.track.nationality.sy.er.invalid"),
-	DublinAMG("com.rcs.ind.track.dublinAMG"),
-	VLG("com.rcs.ind.track.vlg"),
-	Normal("com.rcs.ind.track.normal"),
+	Dublin("com.rcs.ind.track.dublin", false),
+	ProtectedCountry("com.rcs.ind.track.protected.country", false),
+	SafeCountry("com.rcs.ind.track.safe.country", false),
+	NationalitySyErValid("com.rcs.ind.track.nationality.sy.er.valid", false),
+	NationalityElse("com.rcs.ind.track.nationality.else", false),
+	NationalitySyErInvalid("com.rcs.ind.track.nationality.sy.er.invalid", false),
+	DublinAMG("com.rcs.ind.track.dublinAMG", true),
+	VLG("com.rcs.ind.track.vlg", true),
+	Normal("com.rcs.ind.track.normal", true)
 	;
 
 	private final String key;
+	private final boolean simplified;
 
-	private TrackEnum(String key) {
+	private TrackEnum(String key, boolean simplified) {
 		this.key = key;
+		this.simplified = simplified;
 	}
 
 	public String getKey() {
 		return key;
+	}
+	
+	public boolean getSimplified() {
+		return simplified;
 	}
 }
