f1720502e83b17dd8c15a1f032b5209e18f2ba74
jrakhman <juniar.rahman@rithm.nl>
2017-11-10 16:58:27 +0700

    Issue #24846. Create a soap web service (server) to receive updates from indigo
    Issue #24847. Modify supervisor martekplace (in progress)

--- src/main/java/com/rcs/ind/common/enums/ApplicationStatusEnum.java
+++ src/main/java/com/rcs/ind/common/enums/ApplicationStatusEnum.java
@@ -10,7 +10,9 @@ public enum ApplicationStatusEnum {
 	PreparationInProgress(5, "com.rcs.ind.application.status.preparation.in.progress", "com.rcs.ind.screener.status.being.prepared"),
 	ReadyForInterview(6, "com.rcs.ind.application.status.ready.for.interview", "com.rcs.ind.screener.status.prepared"),
 	ReviewInProgress(7, "com.rcs.ind.application.status.review.in.progress", "com.rcs.ind.screener.status.in.progress"),
-	Reviewed(8, "com.rcs.ind.application.status.reviewed", "com.rcs.ind.screener.status.finished");
+	Reviewed(8, "com.rcs.ind.application.status.reviewed", "com.rcs.ind.screener.status.finished"),
+	UploadSuccess(9, "com.rcs.ind.application.status.upload.success", "com.rcs.ind.screener.status.upload.success"),
+	UploadFailed(9, "com.rcs.ind.application.status.upload.failed", "com.rcs.ind.screener.status.upload.failed");
 
 	private final int order;
 	private final String key;
