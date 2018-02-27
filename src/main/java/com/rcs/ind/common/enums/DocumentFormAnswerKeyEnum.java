c513784654fd130a5e4f0c09b77ff6f05284108f
jrakhman <juniar@rithm.nl>
2018-02-27 14:34:14 +0700

    Development #25075: Documents are not showing in preparation

--- src/main/java/com/rcs/ind/common/enums/DocumentFormAnswerKeyEnum.java
+++ src/main/java/com/rcs/ind/common/enums/DocumentFormAnswerKeyEnum.java
@@ -9,13 +9,13 @@ public enum DocumentFormAnswerKeyEnum {
 	DOCUMENTEN_AFGHANISTAN("faanmeldformulierdocumenten.documentenafghanistan.welkedocumenten"),
 	DOCUMENTEN_IRAK("faanmeldformulierdocumenten.documentenirak.welkedocumenten"),
 	DOCUMENTEN_IRAN("faanmeldformulierdocumenten.documenteniran.welkedocumenten"),
-	DOCUMENTEN_VANDAAGBIJ("faanmeldformulierdocumenten.documentenvandaagbij.welkedocumenten"),
+	DOCUMENTEN_VANDAAGBIJ("faanmeldformulierdocumenten.welkedocumentenbij.welkedocumenten"),
 	DOCUMENTEN_SYRIA_TRANSLATION("faanmeldformulierdocumenten.documentensyria.welkedocumenten_translation"),
 	DOCUMENTEN_ERITREA_TRANSLATION("faanmeldformulierdocumenten.documenteneritera.welkedocumenten_translation"),
 	DOCUMENTEN_AFGHANISTAN_TRANSLATION("faanmeldformulierdocumenten.documentenafghanistan.welkedocumenten_translation"),
 	DOCUMENTEN_IRAK_TRANSLATION("faanmeldformulierdocumenten.documentenirak.welkedocumenten_translation"),
 	DOCUMENTEN_IRAN_TRANSLATION("faanmeldformulierdocumenten.documenteniran.welkedocumenten_translation"),
-	DOCUMENTEN_VANDAAGBIJ_TRANSLATION("faanmeldformulierdocumenten.documentenvandaagbij.welkedocumenten_translation");
+	DOCUMENTEN_VANDAAGBIJ_TRANSLATION("faanmeldformulierdocumenten.welkedocumentenbij.welkedocumenten_translation");
 
 	private final String key;
 
