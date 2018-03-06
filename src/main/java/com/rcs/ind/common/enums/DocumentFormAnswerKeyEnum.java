57a1f6fc0caa5b70b1065c696b27a5890f92bca8
jrakhman <juniar@rithm.nl>
2018-03-06 18:49:43 +0700

    Development #25109 : Fill in date of issue for passport Development #25108 : Add the date of issue of identity card
    Another fix after tested by ind

--- src/main/java/com/rcs/ind/common/enums/DocumentFormAnswerKeyEnum.java
+++ src/main/java/com/rcs/ind/common/enums/DocumentFormAnswerKeyEnum.java
@@ -4,17 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum DocumentFormAnswerKeyEnum {
-	DOCUMENTEN_SYRIA("faanmeldformulierdocumenten.documentensyria.welkedocumenten"),
-	DOCUMENTEN_ERITREA("faanmeldformulierdocumenten.documenteneritera.welkedocumenten"),
-	DOCUMENTEN_AFGHANISTAN("faanmeldformulierdocumenten.documentenafghanistan.welkedocumenten"),
-	DOCUMENTEN_IRAK("faanmeldformulierdocumenten.documentenirak.welkedocumenten"),
-	DOCUMENTEN_IRAN("faanmeldformulierdocumenten.documenteniran.welkedocumenten"),
 	DOCUMENTEN_VANDAAGBIJ("faanmeldformulierdocumenten.welkedocumentenbij.welkedocumenten"),
-	DOCUMENTEN_SYRIA_TRANSLATION("faanmeldformulierdocumenten.documentensyria.welkedocumenten_translation"),
-	DOCUMENTEN_ERITREA_TRANSLATION("faanmeldformulierdocumenten.documenteneritera.welkedocumenten_translation"),
-	DOCUMENTEN_AFGHANISTAN_TRANSLATION("faanmeldformulierdocumenten.documentenafghanistan.welkedocumenten_translation"),
-	DOCUMENTEN_IRAK_TRANSLATION("faanmeldformulierdocumenten.documentenirak.welkedocumenten_translation"),
-	DOCUMENTEN_IRAN_TRANSLATION("faanmeldformulierdocumenten.documenteniran.welkedocumenten_translation"),
 	DOCUMENTEN_VANDAAGBIJ_TRANSLATION("faanmeldformulierdocumenten.welkedocumentenbij.welkedocumenten_translation"),
 	DOCUMENTEN_DATUMAFGIFTEIDENTITEITSKAART("faanmeldformulierdocumenten.datumafgifteidentiteitskaart"),
 	DOCUMENTEN_DATUMAFGIFTEPASPOORT("faanmeldformulierdocumenten.datumafgiftepaspoort"),
