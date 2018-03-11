b764a17b2267a4ee3500b72b74820c06bb5a131b
jrakhman <juniar@rithm.nl>
2018-03-12 00:20:47 +0700

    Development #25109 : Fill in date of issue for passport Development #25108 : Add the date of issue of identity card Another fix after tested by ind

--- src/main/java/com/rcs/ind/common/enums/DocumentFormAnswerKeyEnum.java
+++ src/main/java/com/rcs/ind/common/enums/DocumentFormAnswerKeyEnum.java
@@ -4,27 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum DocumentFormAnswerKeyEnum {
-	DOCUMENTEN_VANDAAGBIJ("faanmeldformulierdocumenten.welkedocumentenbij.welkedocumenten"),
-	DOCUMENTEN_VANDAAGBIJ_TRANSLATION("faanmeldformulierdocumenten.welkedocumentenbij.welkedocumenten_translation"),
-	DOCUMENTEN_DATUMAFGIFTEIDENTITEITSKAART("faanmeldformulierdocumenten.datumafgifteidentiteitskaart"),
-	DOCUMENTEN_DATUMAFGIFTEPASPOORT("faanmeldformulierdocumenten.datumafgiftepaspoort"),
-	DOCUMENTEN_WELKEINSTANTIEIDENTITEITSKAARTAFGEGEVEN("faanmeldformulierdocumenten.welkeinstantieidentiteitskaartafgegeven"),
-	DOCUMENTEN_WELKEINSTANTIEPASPOORTAFGEGEVEN("faanmeldformulierdocumenten.welkeinstantiepaspoortafgegeven");
+	DOCUMENTEN_VANDAAGBIJ("faanmeldformulierdocumenten.welkedocumentenbij.welkedocumenten", "faanmeldformulierdocumenten.welkedocumentenbij.welkedocumenten_translation"),
+	DOCUMENTEN_DATUMAFGIFTEIDENTITEITSKAART("faanmeldformulierdocumenten.welkedocumentenbij.datumafgifteidentiteitskaart", "faanmeldformulierdocumenten.welkedocumentenbij.datumafgifteidentiteitskaart_translation"),
+	DOCUMENTEN_DATUMAFGIFTEPASPOORT("faanmeldformulierdocumenten.welkedocumentenbij.datumafgiftepaspoort", "faanmeldformulierdocumenten.welkedocumentenbij.datumafgiftepaspoort_translation"),
+	DOCUMENTEN_WELKEINSTANTIEIDENTITEITSKAARTAFGEGEVEN("faanmeldformulierdocumenten.welkedocumentenbij.welkeinstantieidentiteitskaartafgegeven", "faanmeldformulierdocumenten.welkedocumentenbij.welkeinstantieidentiteitskaartafgegeven_translation"),
+	DOCUMENTEN_WELKEINSTANTIEPASPOORTAFGEGEVEN("faanmeldformulierdocumenten.welkedocumentenbij.welkeinstantiepaspoortafgegeven", "faanmeldformulierdocumenten.welkedocumentenbij.welkeinstantiepaspoortafgegeven_translation"),;
 
 	private final String key;
+	private final String translation;
 
-	private DocumentFormAnswerKeyEnum(String key) {
+	private DocumentFormAnswerKeyEnum(String key, String translation) {
 		this.key = key;
+		this.translation = translation;
 	}
 
 	public String getKey() {
 		return key;
 	}
 
-	public static List<String> getFormAnswerKeys() {
-		List<String> list = new ArrayList<String>();
+	public String getTranslation() {
+		return translation;
+	}
+
+	public static List<String> getFormAnswerKeysAndTranslation() {
+		List<String> list = new ArrayList<>();
 		for (DocumentFormAnswerKeyEnum formAnswerKey : DocumentFormAnswerKeyEnum.values()) {
 			list.add(formAnswerKey.getKey());
+			list.add(formAnswerKey.getTranslation());
 		}
 		return list;
 	}
