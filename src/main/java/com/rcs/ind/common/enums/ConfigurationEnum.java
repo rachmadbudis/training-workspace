13d77f3223496b96e48a3b923a3fe7741388c3e4
daniel <daniel@rotterdam-cs.com>
2018-04-25 15:51:26 +0200

    removed LDAP functionality

--- src/main/java/com/rcs/ind/common/enums/ConfigurationEnum.java
+++ src/main/java/com/rcs/ind/common/enums/ConfigurationEnum.java
@@ -14,35 +14,7 @@ public enum ConfigurationEnum {
 	// Configuration key for Session
 	SessionExtend("sessionAutoExtend", "false"),
 	
-	// Configuration key for LDAP settings
-	LDAP_BASE_PROVIDER_URL("LdapBaseProviderUrl"),
-	LDAP_BASE_DN("LdapBaseDn"),
-	
-	LDAP_SECURITY_PRINCIPAL("LdapSecurityPrincipal"),
-	LDAP_SECURITY_CREDENTIALS("LdapSecurityCredentials"),
 
-	LDAP_AGGREGATE_GROUP_FILTER("LdapAggregateGroupFilter"),
-	LDAP_ACTUAL_GROUP_FILTER("LdapActualGroupFilter"),
-	
-	LDAP_AUTH_SEARCH_FILTER("LdapAuthSearchFilter"),
-	LDAP_IMPORT_USER_SEARCH_FILTER("LdapImportUserSearchFilter"),
-	
-	LDAP_USER_UUID_ATTRIBUTE("LdapUserUuidAttribute"),
-	LDAP_USER_SCREEN_NAME_ATTRIBUTE("LdapUserScreenNameAttribute"),
-	LDAP_USER_EMAIL_ATTRIBUTE("LdapUserEmailAttribute"),
-	LDAP_USER_PASSWORD_ATTRIBUTE("LdapUserPasswordAttribute"),
-	LDAP_USER_FIRST_NAME_ATTRIBUTE("LdapUserFirstNameAttribute"),
-	LDAP_USER_MIDDLE_NAME_ATTRIBUTE("LdapUserMiddleNameAttribute"),
-	LDAP_USER_LAST_NAME_ATTRIBUTE("LdapUserLastNameAttribute"),
-	LDAP_USER_FULL_NAME_ATTRIBUTE("LdapUserFullNameAttribute"),
-	LDAP_USER_MEMBEROF_ATTRIBUTE("LdapUserMemberofAttribute"),
-	
-	LDAP_GROUP_NAME_ATTRIBUTE("LdapGroupNameAttribute"),
-	LDAP_GROUP_DESC_ATTRIBUTE("LdapGroupDescAttribute"),
-	LDAP_GROUP_MEMBER_ATTRIBUTE("LdapGroupMemberAttribute"),
-
-	LDAP_SERVER_NAME("LdapServerName"),
-	
 	// Configuration key for tripleform parameter
 	TF_PROJECT("3fProject"),
 	TF_PASSWORD("3fPassword"),
