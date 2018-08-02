1a68104c541470e71f66327f41ecd9b6db033f71
daniel <daniel@rotterdam-cs.com>
2018-08-02 09:25:56 +0200

    working on improving performance

---
+++ src/main/java/com/rcs/ind/common/enums/AsielListEnum.java
@@ -0,0 +1,35 @@
+package com.rcs.ind.common.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum AsielListEnum {
+	RIC_APPLICABLE("Ric Applicable"),
+	TRACKS ("Tracks"),
+	NATIONALITIES ("Nationalities"),
+	LANGUAGES("Language"),
+	CLAIMLAND ("Claimland"),
+	LOCATION("Location");
+	
+	String name;
+	public String getName() {
+		return name;
+	}
+	
+	private AsielListEnum(String name) {
+		this.name = name;
+	}
+
+	private static final Map<String, AsielListEnum> lookup = new HashMap<String, AsielListEnum>();
+
+	static {
+		for (AsielListEnum s : AsielListEnum.values()) {
+			lookup.put(s.getName(), s);
+		}
+	}
+	
+	public static AsielListEnum fromName(String name) {
+		return lookup.get(name);
+	}
+	
+}
