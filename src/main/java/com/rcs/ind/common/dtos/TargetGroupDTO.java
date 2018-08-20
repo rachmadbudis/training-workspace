5549b0784612014579110626cd7d7703f11aad23
jrakhman <juniar@rithm.nl>
2018-08-20 12:29:36 +0700

    working on hvz & mission marketplace

---
+++ src/main/java/com/rcs/ind/common/dtos/TargetGroupDTO.java
@@ -0,0 +1,22 @@
+package com.rcs.ind.common.dtos;
+
+public class TargetGroupDTO {
+    private Long targetGroupId;
+    private String name;
+
+    public Long getTargetGroupId() {
+        return targetGroupId;
+    }
+
+    public void setTargetGroupId(Long targetGroupId) {
+        this.targetGroupId = targetGroupId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
