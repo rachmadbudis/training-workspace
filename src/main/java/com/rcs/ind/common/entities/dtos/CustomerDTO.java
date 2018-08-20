979b908afb7823eb8f1ca655c91ecaa4662c6245
jrakhman <juniar@rithm.nl>
2018-08-20 16:17:52 +0700

    working on hvz & mission marketplace

--- src/main/java/com/rcs/ind/common/entities/dtos/CustomerDTO.java
+++ src/main/java/com/rcs/ind/common/entities/dtos/CustomerDTO.java
@@ -6,6 +6,26 @@ public class CustomerDTO extends GenericEntityDTO {
 
 	private Long customerId;
 
+	private boolean ricApplicable;
+
+	private String caseNumber;
+
+	private String vNumber;
+
+	private String firstName;
+
+	private String birthDate;
+
+	private String lastName;
+
+	private String birthPlace;
+
+	private String residence;
+
+	private boolean withdrawn;
+
+	private boolean postponed;
+
 	public CustomerDTO() {
 	}
 
@@ -19,5 +39,85 @@ public class CustomerDTO extends GenericEntityDTO {
 
 	public void setCustomerId(Long customerId) {
 		this.customerId = customerId;
+	}
+
+	public boolean isRicApplicable() {
+		return ricApplicable;
+	}
+
+	public void setRicApplicable(boolean ricApplicable) {
+		this.ricApplicable = ricApplicable;
+	}
+
+	public String getCaseNumber() {
+		return caseNumber;
+	}
+
+	public void setCaseNumber(String caseNumber) {
+		this.caseNumber = caseNumber;
+	}
+
+	public String getvNumber() {
+		return vNumber;
+	}
+
+	public void setvNumber(String vNumber) {
+		this.vNumber = vNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getBirthPlace() {
+		return birthPlace;
+	}
+
+	public void setBirthPlace(String birthPlace) {
+		this.birthPlace = birthPlace;
+	}
+
+	public String getResidence() {
+		return residence;
+	}
+
+	public void setResidence(String residence) {
+		this.residence = residence;
+	}
+
+	public boolean isWithdrawn() {
+		return withdrawn;
+	}
+
+	public void setWithdrawn(boolean withdrawn) {
+		this.withdrawn = withdrawn;
+	}
+
+	public boolean isPostponed() {
+		return postponed;
+	}
+
+	public void setPostponed(boolean postponed) {
+		this.postponed = postponed;
 	}
 }
