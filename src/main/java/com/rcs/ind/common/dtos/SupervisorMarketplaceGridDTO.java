58abff5f39aa280ca149c1bd9dc968e5710adc0d
daniel <daniel@rotterdam-cs.com>
2018-10-17 11:47:31 +0200

    working on supervisor and screener marketplace changes

--- src/main/java/com/rcs/ind/common/dtos/SupervisorMarketplaceGridDTO.java
+++ src/main/java/com/rcs/ind/common/dtos/SupervisorMarketplaceGridDTO.java
@@ -19,6 +19,7 @@ public class SupervisorMarketplaceGridDTO  {
 	private Date startDate;
 	private String nationality;
 	private Locale language;
+	private Date creationDate;
 	
 	public String getvNumber() {
 		return vNumber;
@@ -100,6 +101,14 @@ public class SupervisorMarketplaceGridDTO  {
 
 	public void setTrackLabel(String trackLabel) {
 		this.trackLabel = trackLabel;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
 	}
 	
 	}
