e5e5308e20c0d170616f5c30d17dbd305cf0f8ef
daniel <daniel@rotterdam-cs.com>
2018-11-21 16:55:35 +0100

    changed rules for docx generation

--- src/main/java/com/rcs/ind/common/enums/SettlementResultEnum.java
+++ src/main/java/com/rcs/ind/common/enums/SettlementResultEnum.java
@@ -43,7 +43,7 @@ public enum SettlementResultEnum {
     	else return EMPTY;
     }
     
-    public static List<SettlementResultEnum> ignoredInWorklist(){
+    public static List<SettlementResultEnum> negativeResult(){
     	return Arrays.asList(NIET_TOELATING,INTREKKING_DOOR_KLANT,OVERLIJDEN,ONTERECHTE_OPVOERING);
     }
 }
