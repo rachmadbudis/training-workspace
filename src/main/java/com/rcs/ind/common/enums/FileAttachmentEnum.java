1ea1ebd1c7de58d454ee730a3a7fd7c67d69e0e9
daniel <daniel@rotterdam-cs.com>
2018-12-12 18:46:02 +0100

    added indigo name for go packages

--- src/main/java/com/rcs/ind/common/enums/FileAttachmentEnum.java
+++ src/main/java/com/rcs/ind/common/enums/FileAttachmentEnum.java
@@ -13,7 +13,7 @@ public enum FileAttachmentEnum {
 	NAREIS_PROFILE_DOC_PDF_NAME("%s_Nareis_Profile_Document.pdf","com.rcs.ind.filename.nareis.profile","Formulier persoonsgegevens tbv Nareis"),
 	EMM_XML("Klantformulier_%s.xml","com.rcs.ind.filename.emm.xml",""),
 	STATISTIC_REPORT("stats_report_%s.xlsx", "com.rcs.ind.filename.statistic.report.xls",""),
-	HVZ_RIC_PACKAGE("", "com.rcs.ind.filename.hvz.attachment",""),
+	HVZ_RIC_PACKAGE("", "com.rcs.ind.filename.hvz.attachment","Herkomst indicatie"),
 	HVZ_INTERVIEW("%s_Gehoor.docx", "com.rcs.ind.filename.hvz.resettlement.interview", "Gehoor Hervestiging"),
 	HVZ_DECISION_RESULT("%s_Beschikking.docx", "com.rcs.ind.filename.hvz.decision.result", "Beschikking Uitgenodigde Vluchteling"),
 	HVZ_MINUTES("%s_Minuut.docx", "com.rcs.ind.filename.hvz.resettlement.minutes", "Minuut Hervestiging"),
