136c76415675e6699563b135984efc72292456ab
jrakhman <juniar@rithm.nl>
2019-04-05 18:11:17 +0700

    working on #25843

--- src/main/java/com/rcs/ind/common/dtos/MissionFileDTO.java
+++ src/main/java/com/rcs/ind/common/dtos/MissionFileDTO.java
@@ -3,13 +3,22 @@ package com.rcs.ind.common.dtos;
 import com.rcs.ind.common.entities.dtos.GeneralDocumentDTO;
 
 public class MissionFileDTO extends GeneralDocumentDTO {
-    private String note;
+    private String status;
+    private String notes;
 
-    public String getNote() {
-        return note;
+    public String getStatus() {
+        return status;
     }
 
-    public void setNote(String note) {
-        this.note = note;
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
     }
 }
