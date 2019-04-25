136e764a651a3cfa61596eecfd41bc08b5cd96cd
jrakhman <juniar@rithm.nl>
2019-04-25 16:32:42 +0700

    working on #26035 : Fixed passing list of TargetGroup to controller.

---
+++ src/main/java/com/rcs/ind/common/dtos/TargetGroupListDTO.java
@@ -0,0 +1,16 @@
+package com.rcs.ind.common.dtos;
+
+import java.util.List;
+
+public class TargetGroupListDTO {
+
+    private List<TargetGroupDTO> targetGroups;
+
+    public List<TargetGroupDTO> getTargetGroups() {
+        return targetGroups;
+    }
+
+    public void setTargetGroups(List<TargetGroupDTO> targetGroups) {
+        this.targetGroups = targetGroups;
+    }
+}
